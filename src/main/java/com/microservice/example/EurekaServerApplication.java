3f05a851c22ee9f933686651ead87042f7a16fbb
kushalgoswami1993 <kushalgoswami1993@gmail.com>
2025-04-10 17:04:26 +0530

    Update EurekaServerApplication.java
    test

--- src/main/java/com/microservice/example/EurekaServerApplication.java
+++ src/main/java/com/microservice/example/EurekaServerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+	
+	//test for service
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
